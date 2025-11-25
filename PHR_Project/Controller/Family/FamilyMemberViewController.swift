c9a1cc9505a18d137aa77504c07d9c212c8c51c9
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-11-25 16:41:43 +0530

    setup family member details screen redirect

---
+++ PHR_Project/Controller/Family/FamilyMemberViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FamilyMemberViewController.swift
+//  PHR_Project
+//
+//  Created by SDC_USER on 25/11/25.
+//
+
+import UIKit
+
+class FamilyMemberViewController: UIViewController {
+    
+    var familyMember: FamilyMember?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
