8f0f6a5fc496233ad0fccbcba86486047b10cdac
Sanchita Toshniwal <sanchita.tosh@gmail.com>
2025-11-26 14:01:16 +0530

    updates

---
+++ PHR_Project/DocumentsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DocumentsViewController.swift
+//  PHR_Project
+//
+//  Created by SDC-USER on 25/11/25.
+//
+import UIKit
+
+class DocumentsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var documentTableView: UITableView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+   
+    }
