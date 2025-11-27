0fd0ad290319e5551b138781af79f04fdddf1b66
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-11-27 11:35:18 +0530

    Completed profile and started with allergies screen

---
+++ PHR_Project/View/Profile/AllergyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AllergyTableViewCell.swift
+//  PHR_Project
+//
+//  Created by SDC_USER on 27/11/25.
+//
+
+import UIKit
+
+class AllergyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
