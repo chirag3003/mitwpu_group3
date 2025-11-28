02c111b50447814a7dd5439b08f59530d2c33070
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-11-28 16:37:04 +0530

    Integrated home service

--- PHR_Project/Controller/HomeViewController.swift
+++ PHR_Project/Controller/HomeViewController.swift
@@ -7,45 +7,60 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, ProfileServiceDelegate {
 
+    @IBOutlet weak var greetingsLabel: UILabel!
     @IBOutlet weak var mainStack: UIStackView!
     @IBOutlet weak var notificationView: UIView!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var mealLogCardView: UIView!
     @IBOutlet weak var symptomLogCard: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //Add rounded corners
         notificationView.addRoundedCorner(radius: 16)
         mealLogCardView.addRoundedCorner()
         symptomLogCard.addRoundedCorner()
-        
+
         //Adding views to stack
 
         //Adding Glass effect to components
         headerView.applyLiquidGlassEffect()
-        
-        print("HI")
-        print(headerView.frame.size)
+
         //Adding custom spacing between stack items
         mainStack.setCustomSpacing(20, after: notificationView)
 
         // Do any additional setup after loading the view.
         headerView.layer.zPosition = 2
+
+        // Setting up delegates
+        ProfileService.shared.addDeletegate(self)
+
+        // Setting up data
+        greetingsLabel.text =
+            "Good Morning, \(ProfileService.shared.getProfile().firstName)"
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         // Hide the navigation bar
         navigationController?.setNavigationBarHidden(true, animated: animated)
+
+        greetingsLabel.text =
+            "Good Morning, \(ProfileService.shared.getProfile().firstName)"
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // Show the navigation bar for the next screen
         navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    func reloadData() {
+        greetingsLabel.text =
+            "Good Morning, \(ProfileService.shared.getProfile().firstName)"
     }
 
 }
