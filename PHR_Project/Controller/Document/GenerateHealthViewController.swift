7dc17a77b31b14d31d579fcb599f41e0f5d49d9c
Sanchita Toshniwal <sanchita.tosh@gmail.com>
2025-12-09 10:36:37 +0530

    updated

---
+++ PHR_Project/Controller/Document/GenerateHealthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateHealthViewController.swift
+//  PHR_Project
+//
+//  Created by SDC-USER on 28/11/25.
+//
+
+import UIKit
+class GenerateHealthViewController: UIViewController {
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+        
+    }
+   }
+
