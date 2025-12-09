f04fadb585f77e1b7cd901ba4c9b6b37327d3607
Sanchita Toshniwal <sanchita.tosh@gmail.com>
2025-12-09 17:11:51 +0530

    generate health report

--- PHR_Project/Controller/Document/GenerateHealthViewController.swift
+++ PHR_Project/Controller/Document/GenerateHealthViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 class GenerateHealthViewController: UIViewController {
+    
+    @IBOutlet weak var dateInputView: UIStackView!
+    
+    @IBOutlet weak var TypeOfVisitView: UIView!
+    
+    @IBOutlet weak var DataFields: UIView!
+    @IBOutlet weak var fromDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var AdditionalNotes: UIView!
     override func viewDidLoad() {
     
         super.viewDidLoad()
-        
+        dateInputView.addRoundedCorner()
+        TypeOfVisitView.addRoundedCorner()
+        DataFields.addRoundedCorner()
+        AdditionalNotes.addRoundedCorner()
+        TextField.addRoundedCorner(radius:10
+        )
         
     }
-   }
+    @IBAction func toggleSelection(_ sender: UIButton) {
+       
+            sender.isSelected.toggle()
+            
+            // Optional: Print which one was clicked based on the label next to it?
+            // In a real app, you might want to tag your buttons 0-5 in storyboard to identify them.
+            if sender.isSelected {
+                print("Item Selected")
+            } else {
+                print("Item Deselected")
+            }
+    }
+}
+
 
