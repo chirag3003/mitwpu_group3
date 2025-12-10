e3d9c7f079eadc91b6085fb2e455b8c46b1016e5
Sushant Pulipati <sushantpulipati435@gmail.com>
2025-12-10 12:44:58 +0530

    completed glucose trends page

--- PHR_Project/Controller/Meals/AddMealModalViewController.swift
+++ PHR_Project/Controller/Meals/AddMealModalViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class AddMealModalViewController: UIViewController {
 
+    @IBOutlet weak var mealType: UIButton!
     @IBOutlet weak var viewThree: UIView!
     @IBOutlet weak var viewTwo: UIView!
     @IBOutlet weak var viewOne: UIView!
     
+    var selectedMeal: String?
+    
     @IBOutlet weak var notesTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupMealMenu()
         
         viewOne.addRoundedCorner()
         viewTwo.addRoundedCorner()
@@ -35,6 +40,22 @@ class AddMealModalViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func setupMealMenu() {
+        let options = ["Breakfast", "Lunch", "Dinner"]
+        var actions: [UIAction] = []
+
+        for option in options {
+            actions.append(
+                UIAction(title: option) { [weak self] action in
+                    self?.selectedMeal = action.title
+                    self?.mealType.setTitle(action.title, for: .normal)
+                }
+            )
+        }
+        mealType.menu = UIMenu(children: actions)
+        mealType.showsMenuAsPrimaryAction = true
+    }
     
     @IBAction func doneButton(_ sender: Any) {
         dismiss(animated: true)
