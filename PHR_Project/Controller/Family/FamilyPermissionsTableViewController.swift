08deaa52f678d1a618df8ccefc5b4dc243142d1d
VedVC <ved.v.chavan@gmail.com>
2025-12-17 14:54:11 +0530

    family permission page is also now a table view

---
+++ PHR_Project/Controller/Family/FamilyPermissionsTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  FamilyPermissionsTableViewController.swift
+//  PHR_Project
+//
+//  Created by SDC_USER on 17/12/25.
+//
+
+import UIKit
+
+class FamilyPermissionsTableViewController: UITableViewController {
+
+    
+    @IBOutlet weak var readWriteSwitch: UISwitch!
+    @IBOutlet weak var readOnlySwitch: UISwitch!
+    @IBOutlet weak var documentsSwitch: UISwitch!
+    @IBOutlet weak var mealLogsSwitch: UISwitch!
+    @IBOutlet weak var symptomLogsSwitch: UISwitch!
+    @IBOutlet weak var trendsSwitch: UISwitch!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func tickButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+
+    
+    // MARK: - Custom Header for "Turn On All"
+    // MARK: - Custom Header for "Turn On All"
+        override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            
+            if section == 1 {
+                let headerView = UIView()
+                headerView.frame = CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40)
+                
+                // 1. Title Label ("ALLOW ACCESS TO")
+                let label = UILabel()
+                // Standard iOS headers are often Uppercase. Remove .uppercased() if you prefer Title Case.
+                label.text = "Allow Access To"
+                label.font = UIFont.systemFont(ofSize: 17, weight: .medium) // Standard Header Font
+                label.textColor = .secondaryLabel // Standard Gray Color
+                label.translatesAutoresizingMaskIntoConstraints = false
+                
+                // 2. The Button ("Turn On All")
+                let button = UIButton(type: .system)
+                button.setTitle("Turn On All", for: .normal)
+                button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+                button.setTitleColor(.systemBlue, for: .normal)
+                button.addTarget(self, action: #selector(turnOnAllTapped), for: .touchUpInside)
+                button.translatesAutoresizingMaskIntoConstraints = false
+                
+                headerView.addSubview(label)
+                headerView.addSubview(button)
+                
+                // Constraints
+                NSLayoutConstraint.activate([
+                    // CHANGED: Reduced from 20 to 16 to move it slightly Left
+                    label.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16),
+                    label.bottomAnchor.constraint(equalTo: headerView.bottomAnchor, constant: -8),
+                    
+                    // Button: Trailing 16 from right
+                    button.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -16),
+                    button.firstBaselineAnchor.constraint(equalTo: label.firstBaselineAnchor)
+                ])
+                
+                return headerView
+            }
+            
+            return super.tableView(tableView, viewForHeaderInSection: section)
+        }
+        
+        override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            // 40 is the standard height that gives enough breathing room for the text
+            return section == 1 ? 40 : 40
+        }
+
+        // MARK: - Actions
+        
+        @objc func turnOnAllTapped() {
+            // Animate all switches to ON
+            documentsSwitch.setOn(true, animated: true)
+            mealLogsSwitch.setOn(true, animated: true)
+            symptomLogsSwitch.setOn(true, animated: true)
+            trendsSwitch.setOn(true, animated: true)
+        }
+
+    
+
+}
