51ee34693d690cf5ec6ff16b570b60ae87bae65b
Sushant Pulipati <sushantpulipati435@gmail.com>
2026-01-13 10:57:01 +0530

    added glass value functionality

--- PHR_Project/Controller/HomeViewController.swift
+++ PHR_Project/Controller/HomeViewController.swift
@@ -19,6 +19,16 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var mealLogCardView: UIView!
     @IBOutlet weak var symptomLogCard: UIView!
 
+    @IBOutlet weak var glassValue: UILabel!
+    @IBOutlet weak var glassDecrement: UIImageView!
+    @IBOutlet weak var glassIncrement: UIImageView!
+    
+    private var currentGlassCount: Int = 0 {
+        didSet {
+            currentGlassCount = max(0, min(10, currentGlassCount))
+            glassValue.text = "\(currentGlassCount)"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -42,6 +52,8 @@ class HomeViewController: UIViewController {
             "Good Morning, \(ProfileService.shared.getProfile().firstName)"
         stepsSummaryCard.configure(mode: .achievement, progress: 0.45, thickness: 16)
         caloriesSummaryCard.configure(mode: .limitWarning, progress: 0.76, thickness: 16)
+        
+        setupWaterIntakeGestures()
         
         //setting up event listeners
         NotificationCenter.default.addObserver(self, selector: #selector(updateUI), name: NSNotification.Name(NotificationNames.profileUpdated), object: nil)
@@ -70,5 +82,44 @@ class HomeViewController: UIViewController {
     @objc func updateUI() {
         reloadData()
     }
+    
+    private func setupWaterIntakeGestures() {
+            
+            let incrementTap = UITapGestureRecognizer(target: self, action: #selector(incrementGlassCount))
+            glassIncrement.addGestureRecognizer(incrementTap)
+            
+            let decrementTap = UITapGestureRecognizer(target: self, action: #selector(decrementGlassCount))
+            glassDecrement.addGestureRecognizer(decrementTap)
+        }
+    
+    @objc private func incrementGlassCount() {
+            if currentGlassCount < 10 {
+                currentGlassCount += 1
+                animateGlassValue()
+                // Optional: Add haptic feedback
+                let generator = UIImpactFeedbackGenerator(style: .light)
+                generator.impactOccurred()
+            }
+        }
+    
+    @objc private func decrementGlassCount() {
+            if currentGlassCount > 0 {
+                currentGlassCount -= 1
+                animateGlassValue()
+                // Optional: Add haptic feedback
+                let generator = UIImpactFeedbackGenerator(style: .light)
+                generator.impactOccurred()
+            }
+        }
+    
+    private func animateGlassValue() {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.glassValue.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            }) { _ in
+                UIView.animate(withDuration: 0.1) {
+                    self.glassValue.transform = .identity
+                }
+            }
+        }
 
 }
