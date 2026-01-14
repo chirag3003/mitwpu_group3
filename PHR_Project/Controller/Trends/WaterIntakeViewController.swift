85a8cd44e4c8e7606146ae22c78596fce71de707
Sushant Pulipati <sushantpulipati435@gmail.com>
2026-01-14 16:11:01 +0530

    made small changes

---
+++ PHR_Project/Controller/Trends/WaterIntakeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WaterIntakeViewController.swift
+//  PHR_Project
+//
+//  Created by Sushant Pulipati on 14/01/26.
+//
+
+import UIKit
+
+class WaterIntakeViewController: UIViewController {
+
+    @IBOutlet weak var dateCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
