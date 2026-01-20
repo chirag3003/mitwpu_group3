1f35303281edc63dfc877b75b7d96d5103b99550
VedVC <ved.v.chavan@gmail.com>
2026-01-20 16:22:22 +0530

    glucose chart is now a dynamic chart not a pic

--- PHR_Project/Controller/Trends/GlucoseViewController.swift
+++ PHR_Project/Controller/Trends/GlucoseViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import SwiftUI
+import Combine  
 
 class GlucoseViewController: UIViewController {
 
+    @IBOutlet weak var chartSegmentControl: UISegmentedControl!
     @IBOutlet weak var glucoseValueStack: UIStackView!
     @IBOutlet weak var glucoseGraphStack: UIStackView!
     @IBOutlet weak var postDinnerSecondView: UIView!
@@ -18,13 +21,14 @@ class GlucoseViewController: UIViewController {
     @IBOutlet weak var maxView: UIView!
     @IBOutlet weak var minView: UIView!
     @IBOutlet weak var avgView: UIView!
-    @IBOutlet weak var graphImage: UIImageView!
+    @IBOutlet weak var chartContainerView: UIView!
     @IBOutlet weak var graphView: UIView!
+    private let chartViewModel = ChartViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupChart()
         graphView.addRoundedCorner()
-        graphImage.addRoundedCorner()
+        chartContainerView.addRoundedCorner()
         
         avgView.addRoundedCorner()
         avgView.addDropShadow()
@@ -41,19 +45,45 @@ class GlucoseViewController: UIViewController {
         //custom stack spacing
         glucoseGraphStack.setCustomSpacing(-5, after: glucoseValueStack)
 
+        chartViewModel.updateData(for: .week)
+            
+            // 2. Make sure the visual button matches the data (assuming "Week" is index 1)
+            if let segment = chartSegmentControl {
+                segment.selectedSegmentIndex = 1
+            }
         // Do any additional setup after loading the view.
         
     }
     
+    @IBAction func timeSegmentChanged(_ sender: UISegmentedControl) {
+            switch sender.selectedSegmentIndex {
+            case 0: // Day (D)
+                chartViewModel.updateData(for: .day)
+            case 1: // Week (W)
+                chartViewModel.updateData(for: .week)
+            case 2: // Month (M)
+                chartViewModel.updateData(for: .month)
+            case 3: // 6 Months (6M)
+                chartViewModel.updateData(for: .sixMonth)
+            case 4: // Year (Y)
+                chartViewModel.updateData(for: .year)
+            default:
+                break
+            }
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    func setupChart() {
+            // 2. Pass the ViewModel to the SwiftUI View
+            let chartView = GlucoseChartView(viewModel: chartViewModel)
+            
+            let hostingController = UIHostingController(rootView: chartView)
+            
+            addChild(hostingController)
+            hostingController.view.frame = chartContainerView.bounds
+            hostingController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            hostingController.view.backgroundColor = UIColor.clear
+            
+            chartContainerView.addSubview(hostingController.view)
+            hostingController.didMove(toParent: self)
+        }
 }
