a2b8ccd48584e267457606aa9baf01dbd2df627b
VedVC <ved.v.chavan@gmail.com>
2026-01-23 14:03:10 +0530

    sundar pfp updated

--- PHR_Project/DataService/FamilyService.swift
+++ PHR_Project/DataService/FamilyService.swift
@@ -7,12 +7,10 @@ class FamilyService {
 
     private init() {
         familyMembers = [
-            FamilyMember(name: "Dad", imageName: "person.fill", isMe: true),
-            FamilyMember(name: "Mom", imageName: "person.fill", isMe: false),
-            FamilyMember(name: "Ved", imageName: "person.fill", isMe: false),
-            FamilyMember(name: "Sushi", imageName: "person.fill", isMe: false),
-            FamilyMember(name: "Chintu", imageName: "person.fill", isMe: false),
-            FamilyMember(name: "Tosh", imageName: "person.fill", isMe: false),
+            FamilyMember(name: "Ved", imageName: "https://drive.google.com/file/d/1LcEPDDxhZR6i2wA9wFO75791Fg1JwfAW/view?usp=share_link", isMe: false),
+            FamilyMember(name: "Sushant", imageName: "https://drive.google.com/file/d/1TNlQlNRhf6aFEKhTDDBI9IMN7z71qGI6/view?usp=share_link", isMe: false),
+            FamilyMember(name: "Chirag", imageName: "https://drive.google.com/file/d/1Gl6qxbMPs8FVeRFuCid5I1xhoImE4Jak/view?usp=share_link", isMe: false),
+            FamilyMember(name: "Sanchita", imageName: "https://drive.google.com/file/d/1rrCbEMetZk0aHQtbPRnG80pJuE8oYZ5h/view?usp=share_link", isMe: false),
         ]
     }
     
