7d8f51e75d69bff2cfac697a1800bbfcee915049
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2026-01-23 11:46:10 +0530

    Refactored data source

--- PHR_Project/Model/DataSource.swift
+++ PHR_Project/Model/DataSource.swift
@@ -1,14 +1,7 @@
 import Foundation
 
 struct DataSource {
-    var profile: Profile
     let document: Document
-    let family: Family
-    let symptoms: Symptoms
-}
-
-struct Profile {
-    let allergies: [Allergy]
 }
 
 struct Document {
@@ -17,15 +10,6 @@ struct Document {
     let prescriptionData : [PrescriptionModel]
 }
 
-struct Family {
-    let members: [FamilyMember]
-    let contacts: [Contact]
-}
-
-struct MealScreen {
-    let mealItems: [MealItem]
-    let mealDetails: [MealDetails]
-}
 
 func getAllData() -> DataSource {
     // Helper to build a concrete Date from components
@@ -39,15 +23,7 @@ func getAllData() -> DataSource {
         return Calendar.current.date(from: comps) ?? Date()
     }
 
-    return DataSource(
-        profile: Profile(
-            allergies: [
-                Allergy(id: nil, name: "Peanuts", severity: "High", notes: "Difficulty in Breathing"),
-                Allergy(id: nil, name: "Dust", severity: "Medium", notes: "Causes sneezing, runny nose"),
-                Allergy(id: nil, name: "Pollen", severity: "Low", notes: "Seasonal allergy during spring")
-            ]
-        ),
-        document: Document(
+    return DataSource(        document: Document(
             prescriptions: [
                 documentsModel(id: UUID(), title: "Dr. Abhishek Khare", lastUpdatedAt: "18 Nov 2025"),
                 documentsModel(id: UUID(), title: "Dr. Rutuja Khare", lastUpdatedAt: "7 Nov 2025")
@@ -84,41 +60,6 @@ func getAllData() -> DataSource {
                 )
             ]
         ),
-        family: Family(
-            members: [
-                FamilyMember(name: "Dad", imageName: "person.fill", isMe: true),
-                FamilyMember(name: "Mom", imageName: "person.fill", isMe: false),
-                FamilyMember(name: "Ved", imageName: "person.fill", isMe: false),
-                FamilyMember(name: "Sushi", imageName: "person.fill", isMe: false),
-                FamilyMember(name: "Chintu", imageName: "person.fill", isMe: false),
-                FamilyMember(name: "Tosh", imageName: "person.fill", isMe: false)
-            ],
-            contacts: [
-                Contact(name: "Chirag", image: "", phoneNum: "+91 7044521050"),
-                Contact(name: "Sakshi", image: "", phoneNum: "+91 9970001033"),
-                Contact(name: "Ved", image: "", phoneNum: "+91 9284612186")
-            ]
-        ),
-        symptoms: Symptoms(
-            allSymptoms: [
-                Symptom(
-                    id: UUID(),
-                    symptomName: "Fever",
-                    intensity: "High",
-                    dateRecorded: makeDate(year: 2025, month: 11, day: 16, hour: 19, minute: 30),
-                    notes: "Felt chills in the evening",
-                    time: DateComponents(hour: 19, minute: 30)
-                ),
-                Symptom(
-                    id: UUID(),
-                    symptomName: "Headache",
-                    intensity: "Medium",
-                    dateRecorded: makeDate(year: 2025, month: 11, day: 17, hour: 10, minute: 15),
-                    notes: "Throbbing pain, relieved by rest",
-                    time: DateComponents(hour: 10, minute: 15)
-                )
-            ]
-        )
     )
 }
 
